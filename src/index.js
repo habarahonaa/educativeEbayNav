be88cad0e7a103102383248dd3899f3c5c4c48b6
habarahonaa <67131053+habarahonaa@users.noreply.github.com>
2021-12-18 23:38:57 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.querySelector(".menu").innerHTML = `
+<ul>
+<li class=""menu__item menu__item--home><a href="#home">Home</a></li>
+<li class=""menu__item menu__item--motors><a href="#motors">Motors</a></li>
+<li class=""menu__item menu__item--fashion><a href="#fashion">Fashion</a></li>
+<li class=""menu__item menu__item--electronics><a href="#electronics">Electronics</a></li>
+<li class=""menu__item menu__item--toys><a href="#toys">Toys</a></li>
+</ul>
+`;
